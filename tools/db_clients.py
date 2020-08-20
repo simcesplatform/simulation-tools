719fb98c230b134b7427a1da4f7acaee2962114b
Ville Heikkilä <ville.heikkila@tuni.fi>
2020-08-20 20:14:26 +0300

    Added metadata db updates for simulations.

--- tools/db_clients.py
+++ tools/db_clients.py
@@ -2,6 +2,9 @@
 
 """This module contains a class for writing and reading documents to a Mongo database."""
 
+import datetime
+import operator
+
 import pymongo
 
 from tools.datetime_tools import to_utc_datetime_object
@@ -47,22 +50,42 @@ class MongodbClient:
     DEFAULT_ENV_VARIABLE_PREFIX = "MONGODB_"
     CONNECTION_PARAMTERS = ["host", "port", "username", "password", "appname", "tz_aware"]
     TOPIC_ATTRIBUTE = "Topic"
+
+    # These attributes will be converted to datetime objects before writing to database.
     DATETIME_ATTRIBUTES = ["Timestamp", "StartTime", "EndTime"]
+
+    # List of possible metadata attributes in addition to the simulation id.
+    # Each element is a tuple (attribute_name, attribute_types, comparison_operator)
+    # attribute_types is 1-element list for non-list-attribute types and
+    # n-element list for list-attribute types with the last element describing the internal type.
+    # For attributes with comparison_operator is None, the previous value is always overwritten,
+    # for other attributes, the previous value is overwritten only if old_value <comparison_operator> new_value.
+    METADATA_ATTRIBUTES = [
+        ("StartTime", [datetime.datetime], operator.gt),
+        ("EndTime", [datetime.datetime], operator.lt),
+        ("Name", [str], None),
+        ("Description", [str], None),
+        ("Epochs", [int], operator.lt),
+        ("Processes", [list, str], None)
+    ]
 
     def __init__(self, **kwargs):
         if not kwargs:
             kwargs = load_config_from_env_variables()
         self.__connection_parameters = MongodbClient.__get_connection_parameters_only(kwargs)
         self.__database_name = kwargs["database"]
-        self.__metadata_collection = kwargs["metadata_collection"]
+        self.__metadata_collection_name = kwargs["metadata_collection"]
         self.__messages_collection_prefix = kwargs["messages_collection_prefix"]
         self.__collection_identifier = kwargs["collection_identifier"]
 
+        # Set up the Mongo database connection and the metadata collection
         self.__mongo_client = pymongo.MongoClient(**self.__connection_parameters)
         self.__mongo_database = self.__mongo_client[self.__database_name]
+        self.__metadata_collection = self.__mongo_database[self.__metadata_collection_name]
 
     def store_message(self, json_document: dict, document_topic=None):
-        """Stores a new JSON message to the database. The used collection is """
+        """Stores a new JSON message to the database. The used collection is determined by
+           the 'simulation_id' attribute in the message."""
         if isinstance(document_topic, str):
             # Adds or replaces the topic attribute in the given JSON document with the parameter document_topic.
             json_document[MongodbClient.TOPIC_ATTRIBUTE] = document_topic
@@ -72,22 +95,109 @@ class MongodbClient:
             LOGGER.warning("Document does not have '{:s}' attribute.".format(self.__collection_identifier))
             return None
 
-        # Convert the selected attributes from type str to datetime.datetime
-        for datetime_attribute in MongodbClient.DATETIME_ATTRIBUTES:
-            if datetime_attribute in json_document and isinstance(json_document[datetime_attribute], str):
-                json_document[datetime_attribute] = to_utc_datetime_object(json_document[datetime_attribute])
-
-        # TODO: update metadata collection if necessary
+        MongodbClient.datetime_attributes_to_objects(json_document)
 
         mongodb_collection = self.__mongo_database[message_collection_name]
         write_result = mongodb_collection.insert_one(json_document)
         return write_result
+
+    def update_metadata(self, simulation_id: str, **attribute_updates):
+        """Creates or updates the metadata information for a simulation."""
+        if not isinstance(simulation_id, str):
+            LOGGER.warning("Given simulation id was not of type str: '{:s}'".format(str(type(simulation_id))))
+            return False
+
+        simple_document = {self.__collection_identifier: simulation_id}
+        metadata_document = self.__metadata_collection.find_one(simple_document)
+
+        # Add a new metadata document.
+        if metadata_document is None:
+            metadata_document = self.get_metadata_json(simple_document, attribute_updates)
+            write_result = self.__metadata_collection.insert_one(metadata_document)
+            return (
+                isinstance(write_result, pymongo.results.InsertOneResult) and
+                write_result.acknowledged
+            )
+
+        # Update previous document.
+        metadata_document = self.get_metadata_json(metadata_document, attribute_updates)
+        write_result = self.__metadata_collection.replace_one(simple_document, metadata_document)
+        return (
+            isinstance(write_result, pymongo.results.UpdateResult) and
+            write_result.acknowledged and
+            write_result.modified_count == 1
+        )
 
     def __get_message_collection(self, json_document: dict):
         """Returns the collection name for the document."""
         if self.__collection_identifier not in json_document:
             return None
         return self.__messages_collection_prefix + json_document[self.__collection_identifier]
+
+    @classmethod
+    def datetime_attributes_to_objects(cls, json_document: dict):
+        """Convert the datetime attributes from type str to datetime.datetime."""
+        for datetime_attribute in MongodbClient.DATETIME_ATTRIBUTES:
+            if datetime_attribute in json_document and isinstance(json_document[datetime_attribute], str):
+                json_document[datetime_attribute] = to_utc_datetime_object(json_document[datetime_attribute])
+
+    def get_metadata_json(self, old_values: dict, new_values: dict):
+        """Returns a validated metadata document. Any attributes that not
+           simulation_id or in METADATA_ATTRIBUTES list are ignored."""
+        if new_values is None:
+            return None
+        if old_values is None:
+            old_values = {}
+
+        simulation_id_old = old_values.get(self.__collection_identifier, None)
+        simulation_id_new = new_values.get(self.__collection_identifier, None)
+        if ((simulation_id_old is None and simulation_id_new is None) or
+                (simulation_id_old is not None and simulation_id_new is not None and
+                 simulation_id_old != simulation_id_new)):
+            return None
+
+        metadata_values = {self.__collection_identifier: simulation_id_old}
+        for metadata_attribute in MongodbClient.METADATA_ATTRIBUTES:
+            attribute_name, attribute_types, comparison_operator = metadata_attribute
+            old_value = old_values.get(attribute_name, None)
+            new_value = new_values.get(attribute_name, None)
+
+            # New value is of proper type.
+            if MongodbClient.__check_value_types(new_value, attribute_types):
+                # Old value is of proper type.
+                if MongodbClient.__check_value_types(old_value, attribute_types):
+                    if comparison_operator is None or comparison_operator(old_value, new_value):
+                        metadata_values[attribute_name] = new_value
+                    else:
+                        metadata_values[attribute_name] = old_value
+                # Old value either does not exist is not of proper type.
+                else:
+                    metadata_values[attribute_name] = new_value
+            # New value does not exist but the old value is usable.
+            elif MongodbClient.__check_value_types(old_value, attribute_types):
+                metadata_values[attribute_name] = old_value
+
+        return metadata_values
+
+    @classmethod
+    def __check_value_types(cls, value, types: list):
+        """Checks that value is of proper type. Used for the metadata attributes."""
+        if value is None:
+            return False
+        if len(types) == 0:
+            return True
+        if len(types) == 1:
+            return isinstance(value, types[0])
+
+        try:
+            for value_element in value:
+                if not cls.__check_value_types(value_element, types[1:]):
+                    return False
+        except TypeError as error:
+            LOGGER.warning("TypeError : {:s}".format(str(error)))
+            return False
+
+        return True
 
     @classmethod
     def __get_connection_parameters_only(cls, connection_config_dict):
