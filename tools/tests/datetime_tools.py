48f8c080dee6388b60c7b21fe7ce6a5b1d7cf270
Ville Heikkilä <ville.heikkila@tuni.fi>
2020-09-11 14:01:45 +0300

    Added pyright config file.

---
+++ tools/tests/datetime_tools.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""Unit tests for the functions in datetime_tools.py."""
+
+import unittest
+
+
+class TestDatetimeTools(unittest.TestCase):
+    """Unit tests for the datetime_tools module."""
+    # TODO: implement the unit tests
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tools/tests/tools.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""Unit tests for the functions in tools.py."""
+
+import unittest
+
+
+class TestTools(unittest.TestCase):
+    """Unit tests for the tools module."""
+    # TODO: implement the unit tests
+
+
+if __name__ == '__main__':
+    unittest.main()
