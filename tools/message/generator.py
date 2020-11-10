33de0e26b630314b4776d95aca566eaafb7a59f6
Ville Heikkilä <ville.heikkila@tuni.fi>
2020-11-10 09:18:09 +0200

    Message generator now throws exception when there is a problem.

--- tools/message/generator.py
+++ tools/message/generator.py
@@ -2,7 +2,7 @@
 
 """This module contains general utils for working with simulation platform message classes."""
 
-from typing import Iterator, List, Optional, Type, Union
+from typing import Iterator, List, Type, Union
 
 from tools.exceptions.messages import MessageError
 from tools.message.abstract import AbstractMessage
@@ -48,8 +48,10 @@ class MessageGenerator:
         """Returns a new AbstractMessage instance."""
         return next(self._abstract_message_generator)
 
-    def get_message(self, message_class: Type[AbstractMessage], **kwargs) -> Optional[AbstractMessage]:
-        """Returns a new message instance of type message_class according to the given parameters."""
+    def get_message(self, message_class: Type[AbstractMessage], **kwargs) -> AbstractMessage:
+        """Returns a new message instance of type message_class according to the given parameters.
+           Throws an MessageError or ValueError exception if there is a problem with the given parameters.
+        """
         # TODO: add unit tests for this function
         if message_class is EpochMessage:
             return self.get_epoch_message(**kwargs)
@@ -61,54 +63,47 @@ class MessageGenerator:
             return self.get_resource_state_message(**kwargs)
 
         if not issubclass(message_class, AbstractMessage):
-            LOGGER.warning("{:s} is not a subclass of {:s}".format(
+            raise MessageError("{:s} is not a subclass of {:s}".format(
                 getattr(message_class, "__name__"), AbstractMessage.__name__))
-            return None
 
         # No predefined function for the given message class type.
         # => Argument checking is done by the message class but no error messages related to extra arguments
         #    are given as in the other cases.
         abstract_message = self.get_abstract_message()
-        try:
-            return message_class(
-                Type=message_class.CLASS_MESSAGE_TYPE,
-                SimulationId=abstract_message.simulation_id,
-                SourceProcessId=abstract_message.source_process_id,
-                MessageId=abstract_message.message_id,
-                Timestamp=abstract_message.timestamp,
-                **kwargs
-            )
-        except (MessageError, ValueError):
-            return None
+        return message_class(
+            Type=message_class.CLASS_MESSAGE_TYPE,
+            SimulationId=abstract_message.simulation_id,
+            SourceProcessId=abstract_message.source_process_id,
+            MessageId=abstract_message.message_id,
+            Timestamp=abstract_message.timestamp,
+            **kwargs
+        )
 
     def get_epoch_message(self, EpochNumber: int, TriggeringMessageIds: List[str],
                           StartTime: str, EndTime: str,
                           LastUpdatedInEpoch: int = None, Warnings: List[str] = None) \
-            -> Optional[EpochMessage]:
+            -> EpochMessage:
         """Returns a new EpochMessage corresponding to the given parameters.
-           Returns None if the message creation was unsuccessful."""
+           Throws an exception if the message creation was unsuccessful."""
         # pylint: disable=invalid-name
         abstract_message = self.get_abstract_message()
-        try:
-            return EpochMessage(
-                Type=EpochMessage.CLASS_MESSAGE_TYPE,
-                SimulationId=abstract_message.simulation_id,
-                SourceProcessId=abstract_message.source_process_id,
-                MessageId=abstract_message.message_id,
-                Timestamp=abstract_message.timestamp,
-                EpochNumber=EpochNumber,
-                TriggeringMessageIds=TriggeringMessageIds,
-                LastUpdatedInEpoch=LastUpdatedInEpoch,
-                Warnings=Warnings,
-                StartTime=StartTime,
-                EndTime=EndTime
-            )
-        except (MessageError, ValueError):
-            return None
+        return EpochMessage(
+            Type=EpochMessage.CLASS_MESSAGE_TYPE,
+            SimulationId=abstract_message.simulation_id,
+            SourceProcessId=abstract_message.source_process_id,
+            MessageId=abstract_message.message_id,
+            Timestamp=abstract_message.timestamp,
+            EpochNumber=EpochNumber,
+            TriggeringMessageIds=TriggeringMessageIds,
+            LastUpdatedInEpoch=LastUpdatedInEpoch,
+            Warnings=Warnings,
+            StartTime=StartTime,
+            EndTime=EndTime
+        )
 
-    def get_status_message(self, Value: str, **kwargs) -> Optional[StatusMessage]:
+    def get_status_message(self, Value: str, **kwargs) -> StatusMessage:
         """Returns a new StatusMessage corresponding to the given parameters.
-           Returns None if the message creation was unsuccessful."""
+           Throws an exception if the message creation was unsuccessful."""
         # pylint: disable=invalid-name
         if Value == StatusMessage.STATUS_VALUES[0]:   # should be "ready"
             return self.get_status_ready_message(**kwargs)
@@ -117,112 +112,97 @@ class MessageGenerator:
 
         # unknown status value, try to create the message regardless
         abstract_message = self.get_abstract_message()
-        try:
-            return StatusMessage(
-                Type=StatusMessage.CLASS_MESSAGE_TYPE,
-                SimulationId=abstract_message.simulation_id,
-                SourceProcessId=abstract_message.source_process_id,
-                MessageId=abstract_message.message_id,
-                Timestamp=abstract_message.timestamp,
-                Value=Value,
-                **kwargs
-            )
-        except (MessageError, ValueError):
-            return None
+        return StatusMessage(
+            Type=StatusMessage.CLASS_MESSAGE_TYPE,
+            SimulationId=abstract_message.simulation_id,
+            SourceProcessId=abstract_message.source_process_id,
+            MessageId=abstract_message.message_id,
+            Timestamp=abstract_message.timestamp,
+            Value=Value,
+            **kwargs
+        )
 
     def get_status_ready_message(self, EpochNumber: int, TriggeringMessageIds: List[str],
                                  LastUpdatedInEpoch: int = None, Warnings: List[str] = None) \
-            -> Optional[StatusMessage]:
+            -> StatusMessage:
         """Returns a new StatusMessage corresponding to the given parameters.
-           Returns None if the message creation was unsuccessful."""
+           Throws an exception if the message creation was unsuccessful."""
         # pylint: disable=invalid-name
         abstract_message = self.get_abstract_message()
-        try:
-            return StatusMessage(
-                Type=StatusMessage.CLASS_MESSAGE_TYPE,
-                SimulationId=abstract_message.simulation_id,
-                SourceProcessId=abstract_message.source_process_id,
-                MessageId=abstract_message.message_id,
-                Timestamp=abstract_message.timestamp,
-                EpochNumber=EpochNumber,
-                TriggeringMessageIds=TriggeringMessageIds,
-                LastUpdatedInEpoch=LastUpdatedInEpoch,
-                Warnings=Warnings,
-                Value=StatusMessage.STATUS_VALUES[0]  # should be "ready"
-            )
-        except (MessageError, ValueError):
-            return None
+        return StatusMessage(
+            Type=StatusMessage.CLASS_MESSAGE_TYPE,
+            SimulationId=abstract_message.simulation_id,
+            SourceProcessId=abstract_message.source_process_id,
+            MessageId=abstract_message.message_id,
+            Timestamp=abstract_message.timestamp,
+            EpochNumber=EpochNumber,
+            TriggeringMessageIds=TriggeringMessageIds,
+            LastUpdatedInEpoch=LastUpdatedInEpoch,
+            Warnings=Warnings,
+            Value=StatusMessage.STATUS_VALUES[0]  # should be "ready"
+        )
 
     def get_status_error_message(self, EpochNumber: int, TriggeringMessageIds: List[str],
                                  Description: str = None,
                                  LastUpdatedInEpoch: int = None, Warnings: List[str] = None) \
-            -> Optional[StatusMessage]:
+            -> StatusMessage:
         """Returns a new StatusMessage corresponding to the given parameters.
-           Returns None if the message creation was unsuccessful."""
+           Throws an exception if the message creation was unsuccessful."""
         # pylint: disable=invalid-name
         abstract_message = self.get_abstract_message()
-        try:
-            return StatusMessage(
-                Type=StatusMessage.CLASS_MESSAGE_TYPE,
-                SimulationId=abstract_message.simulation_id,
-                SourceProcessId=abstract_message.source_process_id,
-                MessageId=abstract_message.message_id,
-                Timestamp=abstract_message.timestamp,
-                EpochNumber=EpochNumber,
-                TriggeringMessageIds=TriggeringMessageIds,
-                LastUpdatedInEpoch=LastUpdatedInEpoch,
-                Warnings=Warnings,
-                Value=StatusMessage.STATUS_VALUES[0],  # should be "ready"
-                Description=Description
-            )
-        except (MessageError, ValueError):
-            return None
+        return StatusMessage(
+            Type=StatusMessage.CLASS_MESSAGE_TYPE,
+            SimulationId=abstract_message.simulation_id,
+            SourceProcessId=abstract_message.source_process_id,
+            MessageId=abstract_message.message_id,
+            Timestamp=abstract_message.timestamp,
+            EpochNumber=EpochNumber,
+            TriggeringMessageIds=TriggeringMessageIds,
+            LastUpdatedInEpoch=LastUpdatedInEpoch,
+            Warnings=Warnings,
+            Value=StatusMessage.STATUS_VALUES[0],  # should be "ready"
+            Description=Description
+        )
 
     def get_simulation_state_message(self, SimulationState: str, Name: str = None, Description: str = None) \
-            -> Optional[SimulationStateMessage]:
+            -> SimulationStateMessage:
         """Returns a new StatusMessage corresponding to the given parameters.
-           Returns None if the message creation was unsuccessful."""
+           Throws an exception if the message creation was unsuccessful."""
         # pylint: disable=invalid-name
         abstract_message = self.get_abstract_message()
-        try:
-            return SimulationStateMessage(
-                Type=SimulationStateMessage.CLASS_MESSAGE_TYPE,
-                SimulationId=abstract_message.simulation_id,
-                SourceProcessId=abstract_message.source_process_id,
-                MessageId=abstract_message.message_id,
-                Timestamp=abstract_message.timestamp,
-                SimulationState=SimulationState,
-                Name=Name,
-                Description=Description
-            )
-        except (MessageError, ValueError):
-            return None
+        return SimulationStateMessage(
+            Type=SimulationStateMessage.CLASS_MESSAGE_TYPE,
+            SimulationId=abstract_message.simulation_id,
+            SourceProcessId=abstract_message.source_process_id,
+            MessageId=abstract_message.message_id,
+            Timestamp=abstract_message.timestamp,
+            SimulationState=SimulationState,
+            Name=Name,
+            Description=Description
+        )
 
     def get_resource_state_message(self, EpochNumber: int, TriggeringMessageIds: List[str], Bus: str,
                                    RealPower: Union[float, QuantityBlock], ReactivePower: Union[float, QuantityBlock],
                                    LastUpdatedInEpoch: int = None, Warnings: List[str] = None,
                                    Node: int = None, StateOfCharge: Union[float, QuantityBlock] = None) \
-            -> Optional[ResourceStateMessage]:
+            -> ResourceStateMessage:
         """Returns a new StatusMessage corresponding to the given parameters.
-           Returns None if the message creation was unsuccessful."""
+           Throws an exception if the message creation was unsuccessful."""
         # pylint: disable=invalid-name
         abstract_message = self.get_abstract_message()
-        try:
-            return ResourceStateMessage(
-                Type=ResourceStateMessage.CLASS_MESSAGE_TYPE,
-                SimulationId=abstract_message.simulation_id,
-                SourceProcessId=abstract_message.source_process_id,
-                MessageId=abstract_message.message_id,
-                Timestamp=abstract_message.timestamp,
-                EpochNumber=EpochNumber,
-                TriggeringMessageIds=TriggeringMessageIds,
-                LastUpdatedInEpoch=LastUpdatedInEpoch,
-                Warnings=Warnings,
-                Bus=Bus,
-                RealPower=RealPower,
-                ReactivePower=ReactivePower,
-                Node=Node,
-                StateOfCharge=StateOfCharge
-            )
-        except (MessageError, ValueError):
-            return None
+        return ResourceStateMessage(
+            Type=ResourceStateMessage.CLASS_MESSAGE_TYPE,
+            SimulationId=abstract_message.simulation_id,
+            SourceProcessId=abstract_message.source_process_id,
+            MessageId=abstract_message.message_id,
+            Timestamp=abstract_message.timestamp,
+            EpochNumber=EpochNumber,
+            TriggeringMessageIds=TriggeringMessageIds,
+            LastUpdatedInEpoch=LastUpdatedInEpoch,
+            Warnings=Warnings,
+            Bus=Bus,
+            RealPower=RealPower,
+            ReactivePower=ReactivePower,
+            Node=Node,
+            StateOfCharge=StateOfCharge
+        )
