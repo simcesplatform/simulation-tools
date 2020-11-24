48e242fa34eeef08266da0265b5e6451b38abeb6
Ville Heikkilä <ville.heikkila@tuni.fi>
2020-11-24 08:21:39 +0200

    Added all Block class imports to tools.messages

--- tools/messages.py
+++ tools/messages.py
@@ -3,16 +3,18 @@
 """This module contains a class for creating and holding messages for the RabbitMQ message bus."""
 
 # import these for backwards compatibility
-from tools.message.abstract import BaseMessage, AbstractMessage     # pylint: disable=unused-import
-from tools.message.abstract import AbstractResultMessage, get_json  # pylint: disable=unused-import
-from tools.message.block import QuantityBlock                       # pylint: disable=unused-import
-from tools.message.epoch import EpochMessage                        # pylint: disable=unused-import
-from tools.message.factory import MessageFactory                    # pylint: disable=unused-import
-from tools.message.general import GeneralMessage, ResultMessage     # pylint: disable=unused-import
-from tools.message.generator import MessageGenerator                # pylint: disable=unused-import
-from tools.message.simulation_state import SimulationStateMessage   # pylint: disable=unused-import
-from tools.message.status import StatusMessage                      # pylint: disable=unused-import
-from tools.message.utils import get_next_message_id                 # pylint: disable=unused-import
+from tools.message.abstract import BaseMessage, AbstractMessage      # pylint: disable=unused-import
+from tools.message.abstract import AbstractResultMessage, get_json   # pylint: disable=unused-import
+from tools.message.block import QuantityBlock                        # pylint: disable=unused-import
+from tools.message.block import ValueArrayBlock, QuantityArrayBlock  # pylint: disable=unused-import
+from tools.message.block import TimeSeriesBlock                      # pylint: disable=unused-import
+from tools.message.epoch import EpochMessage                         # pylint: disable=unused-import
+from tools.message.factory import MessageFactory                     # pylint: disable=unused-import
+from tools.message.general import GeneralMessage, ResultMessage      # pylint: disable=unused-import
+from tools.message.generator import MessageGenerator                 # pylint: disable=unused-import
+from tools.message.simulation_state import SimulationStateMessage    # pylint: disable=unused-import
+from tools.message.status import StatusMessage                       # pylint: disable=unused-import
+from tools.message.utils import get_next_message_id                  # pylint: disable=unused-import
 
 from tools.tools import FullLogger
 
