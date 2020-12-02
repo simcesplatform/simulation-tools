bf5af647b7bc9e940006b3d67b7f5f2471776df6
Ville Heikkilä <ville.heikkila@tuni.fi>
2020-12-02 09:06:26 +0200

    Added an example for CSV to TimeSeriesBlock.

--- examples/messages.py
+++ examples/messages.py
@@ -2,8 +2,10 @@
 
 """This module contains code examples related to using the message classes."""
 
+import csv
 import json
 import logging
+from typing import Dict, Generator, Optional
 
 # the used message examples are defined in examples/message_json.py
 from examples.message_json import status_ready_message, status_error_message, example_message, \
@@ -221,3 +223,146 @@ def test_message_generator():
     except MessageError as message_error:
         LOGGER.warning("Received an exception {}: {}".format(type(message_error), message_error))
     LOGGER.info("")
+
+
+def get_timeseries_from_csv(filename: str,
+                            timestamp_column: str,
+                            unit_of_measures: Dict[str, str],
+                            column_separator: str,
+                            rows: Optional[int] = None) -> Generator[TimeSeriesBlock, None, None]:
+    """
+    Reads a CSV file and converts it to a Time series block object.
+    Returns only block created from the asked number of rows at a time.
+    No error handling regarding file reading errors is done.
+    - filename: the name of the CSV file (the file must contain a header row)
+    - timestamp_column: the column name that is to be used as timestamp list, i.e. the TimeIndex attribute
+    - unit_of_measures: a dictionary containing the units for each value column in the CSV file
+    - column_separator: the character used as a column separator in the CSV file
+    - rows: how many values are taken into the Time series block (if None or missing, the entire file is used)
+    """
+    with open(filename, mode="r", encoding="UTF-8") as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=column_separator)
+
+        if timestamp_column not in csv_reader.fieldnames:
+            LOGGER.warning("No timestamp column found from the CSV file {}".format(filename))
+            # Return None on error.
+            return None
+
+        # The other column names except the timestamp column as a list
+        series_names = [
+            field_name
+            for field_name in csv_reader.fieldnames
+            if field_name != timestamp_column
+        ]
+
+        # Check that the columns from the CSV file matches the given unit of measures
+        if set(series_names) != set(unit_of_measures):
+            LOGGER.warning("The unit of measure list, {}, does not match with the column names, {}".format(
+                list(unit_of_measures), series_names
+            ))
+            return None
+
+        # Setup variables for the time_index and the value series
+        time_index = []
+        series = {
+            series_name: []
+            for series_name in series_names
+        }
+
+        row_count = 0
+        at_the_beginning = True
+        for csv_row in csv_reader:
+            if row_count == 0 and not at_the_beginning:
+                # Clear the variables for new a Time series block
+                time_index = []
+                series = {
+                    series_name: []
+                    for series_name in series_names
+                }
+
+            row_count += 1
+            time_index.append(csv_row[timestamp_column])
+            for series_name in series_names:
+                # assume all the values for the time series are floats
+                # NOTE: no error checking if the value is not convertable
+                series[series_name].append(float(csv_row[series_name]))
+
+            if rows is not None and row_count == rows:
+                row_count = 0
+                at_the_beginning = False
+                # After reading the wanted number of rows return a Time series block object.
+                yield TimeSeriesBlock(
+                    TimeIndex=time_index,
+                    Series={
+                        series_name: ValueArrayBlock(  # with float values also QuantityArrayBlock could be used
+                            Values=series_values,
+                            UnitOfMeasure=unit_of_measures[series_name]
+                        )
+                        for series_name, series_values in series.items()
+                    }
+                )
+
+    # Return the last rows as a time series block even if there were not enough rows left.
+    # NOTE: this is also used to return the Time series block object if the entire CSV file
+    # is wanted (when rows is None or it is missing) as a single Time series block object.
+    if row_count > 0 or rows is None:
+        yield TimeSeriesBlock(
+            TimeIndex=time_index,
+            Series={
+                series_name: ValueArrayBlock(
+                    Values=series_values,
+                    UnitOfMeasure=unit_of_measures[series_name]
+                )
+                for series_name, series_values in series.items()
+            }
+        )
+
+    return None
+
+
+def test_timeseries_from_csv():
+    """Examples for creating time series blocks from a csv file."""
+    single_value_csv_filename = "examples/timeseries_single.csv"
+    multi_value_csv_filename = "examples/timeseries_multi.csv"
+    column_separator = ";"
+    timestamp_column = "timestamp"
+
+    # load an entire CSV file to a Time series block object
+    timeseries_single = next(get_timeseries_from_csv(
+        filename=single_value_csv_filename,
+        timestamp_column=timestamp_column,
+        unit_of_measures={"Cargo": "g"},
+        column_separator=column_separator
+    ))
+
+    # Print out the Time series block in a human-readable manner
+    LOGGER.info("The Time series block containing a single value series column")
+    LOGGER.info("{}".format(json.dumps(timeseries_single.json(), indent=4)))
+    LOGGER.info("")
+
+    # load a CSV file to a Time series block 6 rows at a time
+    timeseries_multi_generator = get_timeseries_from_csv(
+        filename=multi_value_csv_filename,
+        timestamp_column=timestamp_column,
+        unit_of_measures={
+            "PlaceA": "Cel",
+            "PlaceB": "Cel"
+        },
+        column_separator=column_separator,
+        rows=6
+    )
+    for index, timeseries_multi in enumerate(timeseries_multi_generator, start=1):
+        LOGGER.info("The Time series block {} from the multi column CSV".format(index))
+        LOGGER.info("{}".format(json.dumps(timeseries_multi.json(), indent=4)))
+
+    # Use the Time series blocks as the attribute values for a message object
+    example = ExampleMessage.from_json(example_message)
+    # Use the latest multi column series as the value for Temperature attribute in ExampleMessage
+    example.temperature = timeseries_multi  # type: ignore  # pylint: disable=undefined-loop-variable
+    # Use the single column series as the value for Weight attribute in ExampleMessage
+    example.weight = timeseries_single
+
+    # Output the message as JSON in a readable format
+    LOGGER.info("")
+    LOGGER.info("The example message with new Temperature and Weight attributes:")
+    LOGGER.info(json.dumps(example.json(), indent=4))
